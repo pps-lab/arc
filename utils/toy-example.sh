1808378df114cc9a38caafc4c46b8b490afa5436
Alexander Mandt <amandt@student.ethz.ch>
2022-04-07 20:44:37 +0000

    WIP MP-SPDZ integration

---
+++ utils/toy-example.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Go to mp-spdz
+cd mp-spdz
+# compile tutorial
+./compile.py tutorial
+# generate Player data
+echo 1 2 3 4 > Player-Data/Input-P$($1)-0
+# Sleep for sync
+sleep $($2)
+# Execute mascot 
+./mascot-party.x -N "$3" -h "$4" "$1" tutorial 
