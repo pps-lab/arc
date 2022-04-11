c7d2714fab5993f40095a121ccd57a5954eba5dd
Alexander Mandt <amandt@student.ethz.ch>
2022-04-11 18:55:18 +0000

    Updated toy-example util script

--- utils/toy-example.sh
+++ utils/toy-example.sh
@@ -9,4 +9,8 @@ echo 1 2 3 4 > Player-Data/Input-P$($1)-0
 # Sleep for sync
 sleep $($2)
 # Execute mascot 
-./mascot-party.x -N "$3" -h "$4" -p "$1" tutorial > ../results/player-$1-out.txt
+RESULTS=$(./mascot-party.x -N "$3" -h "$4" -p "$1" tutorial | base64)
+cd ..
+
+# Generate Result JSON
+echo "{\"result\": \"$RESULTS\", \"player\": \"$1\"}" > ./results/player-$1-output.json
