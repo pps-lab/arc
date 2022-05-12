e403f9708325db5307fb09069a168bb4b16fa803
Alexander Mandt <amandt@student.ethz.ch>
2022-05-12 16:46:46 +0000

    working on ml-train examples

---
+++ utils/ml-stuff/ml-train-49.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Start of execution: $(date +\"%T.%N\")"
+# save current directory
+MY_CUR_DIR="$(pwd)"
+# Go to code dir
+THE_CODE_DIR="$5"
+cd "$THE_CODE_DIR"
+
+# Goto data-dir
+cd custom-data
+# Extract input data
+unzip mnist-train-input-49.zip 
+# Move Data to Player-Data folder
+mkdir "$THE_CODE_DIR/mp-spdz/Player-Data"
+mv raw-input-49.txt "$THE_CODE_DIR/mp-spdz/Player-Data/Input-P0-0"
+touch "$THE_CODE_DIR/mp-spdz/Player-Data/Input-P1-0"
+cd ..
+
+echo "Completed extraction in: $(date +\"%T.%N\")"
+
+# Copy ml-example.mpc to Programs folder
+cp utils/ml-stuff/ml-train.mpc mp-spdz/Programs/Source/custom-ml-train.mpc
+# The needed program is already in the example programs
+
+# Go to mp-spdz
+cd mp-spdz
+# compile tutorial
+echo "Start of compilation: $(date +\"%T.%N\")"
+./compile.py -R 64 custom-ml-train
+echo "End of compilation: $(date +\"%T.%N\")"
+# Sleep for sync
+sleep "$2"
+# Execute mascot 
+./replicated-ring-party.x -h "$4" -pn 12300 "$1" custom-ml-train  | tee "$MY_CUR_DIR/results/result-$1.txt"
+cd ..
