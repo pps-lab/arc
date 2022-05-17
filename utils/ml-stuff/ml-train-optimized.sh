95b46542968d1b225629afc4da160c747bac8a0f
Alexander Mandt <amandt@student.ethz.ch>
2022-05-17 22:29:01 +0000

    Modified ml-train-example

--- utils/ml-stuff/ml-train-optimized.sh
+++ utils/ml-stuff/ml-train-optimized.sh
@@ -29,10 +29,10 @@ cp utils/ml-stuff/ml-train-optimized.mpc mp-spdz/Programs/Source/custom-ml-train
 cd mp-spdz
 # compile tutorial
 echo "Start of compilation: $(date +\"%T.%N\")"
-./compile.py -R 64 -CD custom-ml-train-optimized "$6" "$7" trunc_pr split3 
+./compile.py -R 64 -CD custom-ml-train-optimized "$6" "${7}" trunc_pr split3 
 echo "End of compilation: $(date +\"%T.%N\")"
 # Sleep for sync
 sleep "$2"
 # Execute mascot 
-./replicated-ring-party.x -h "$4" -pn 12300 "$1" custom-ml-train-optimized-$6-$7-trunc_pr-split3  | tee "$MY_CUR_DIR/results/result-$1.txt"
+./replicated-ring-party.x -h "$4" -pn 12300 "$1" custom-ml-train-optimized-$6-${7}-trunc_pr-split3  | tee "$MY_CUR_DIR/results/result-$1.txt"
 cd ..
