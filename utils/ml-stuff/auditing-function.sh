336b699e6abce527b2cdc9b761b4c2fbe97c9b0e
Alexander Mandt <amandt@student.ethz.ch>
2022-06-05 15:38:04 +0000

    Implemented auditing function experiment v1

---
+++ utils/ml-stuff/auditing-function.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Note: $6 -> Number of threads
+
+echo "Start of execution: $(date +\"%T.%N\")"
+# save current directory
+MY_CUR_DIR="$(pwd)"
+# Go to code dir
+THE_CODE_DIR="$5"
+cd "$THE_CODE_DIR"
+
+# Goto data-dir
+cd custom-data
+# Extract input data
+unzip mnist-audit-conv-10epoch.zip
+# Move Data to Player-Data folder
+mkdir "$THE_CODE_DIR/mp-spdz/Player-Data"
+mv Input-P0-0 "$THE_CODE_DIR/mp-spdz/Player-Data/Input-P0-0"
+touch "$THE_CODE_DIR/mp-spdz/Player-Data/Input-P1-0"
+cd ..
+
+echo "Completed extraction in: $(date +\"%T.%N\")"
+
+# The needed program is already in the example programs
+
+# Go to mp-spdz
+cd mp-spdz
+# compile tutorial
+echo "Start of compilation: $(date +\"%T.%N\")"
+./compile.py -R 64 -C -D ../scripts/auditing_function.mpc "$6" trunc_pr split3 
+echo "End of compilation: $(date +\"%T.%N\")"
+# Sleep for sync
+sleep "$2"
+# Execute mascot 
+./replicated-ring-party.x -h "$4" -pn 12300 "$1" custom-audit-data-import-test-$6-trunc_pr-split3  | tee "$MY_CUR_DIR/results/result-$1.txt"
+
+# Do cleanup
+rm -rf Player-Prep-Data/
+mkdir Player-Prep-Data/
+
+rm -rf Player-Data/
+mkdir Player-Data/
+cd ..
