642a2a4ea7b4c040871b5335a86f42d7bf1a5a2d
Alexander Mandt <amandt@student.ethz.ch>
2022-07-16 20:10:12 +0200

    WIP experiment-runner

--- utils/experiment-runner.sh
+++ utils/experiment-runner.sh
@@ -6,4 +6,4 @@
 
 export PYTHONPATH="$1/utils"
 echo "Does this work"
-$1/.venv/bin/python -m python_utils.scripts.experiment_runner --player-number $2 --sleep-time $3
+$1/.venv/bin/python -m python_utils.scripts.experiment_runner --player-number $2 --sleep-time $3 
--- utils/python_utils/scripts/experiment_runner.py
+++ utils/python_utils/scripts/experiment_runner.py
@@ -4,6 +4,7 @@ import python_utils.input_file_processor as ifp
 import python_utils.output_capture as out_cap
 import click
 import os
+import shutil
 import string
 import random
 
@@ -32,7 +33,14 @@ def process_input(task_config: config_def.TaskConfig):
     file_processor = ifp.InputFileProcessor(task_config.input_file_name)
     file_processor.process_input()
 
+
+def copy_script_to_sources(task_config: config_def.TaskConfig):
+    script_path = os.path.join(task_config.abs_path_to_code_dir, "scripts", f"{task_config.script_name}.mpc")
+    source_path = os.path.join(task_config.abs_path_to_code_dir, "mp-spdz", "Programs", "Source", f"{task_config.script_name}.mpc")
+    shutil.copy(script_path, source_path)
+
 def compile_script_with_args(task_connfig: config_def.TaskConfig):
+    copy_script_to_sources(task_config=task_connfig)
     comp_runner = runner_defs.CompilerRunner(
         script_name=task_connfig.script_name,
         script_args=task_connfig.script_args,
