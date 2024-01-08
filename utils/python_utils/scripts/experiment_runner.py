6f8107796c64cd634a30ff494e56c23987a562ab
Hidde L <mail.hidde@gmail.com>
2024-01-08 14:16:30 +0100

    let mp-spdz choose random point

--- utils/python_utils/scripts/experiment_runner.py
+++ utils/python_utils/scripts/experiment_runner.py
@@ -29,6 +29,7 @@ This module provides the following functionalities:
     This is the entrypoint of the Experiment Runner
 
 """
+import re
 import sys
 import warnings
 
@@ -311,13 +312,14 @@ def convert_shares(task_config):
         eval_point = None
         if task_config.consistency_args.eval_point is not None:
             eval_point = task_config.consistency_args.eval_point
-        elif task_config.consistency_args.single_random_eval_point:
-            # technically this does not use the full range (mod scalar field size) at the moment.
-            print("Generating random evaluation point")
-            eval_point = random.getrandbits(251)
         else:
             warnings.warn("Note that if a party has multiple inputs to prove, we do not support using different points at the moment."
                           "It would be easy to support in the mpc-consistency code.")
+
+        if task_config.consistency_args.single_random_eval_point:
+            print("Using the same eval point across poly eval runs."
+                  "We will run the first eval script first and let the parties agree on a point."
+                  "We then use this point in subsequent invocations.")
 
         input_counter = 0
         for party_id, player_input_counts in player_input_counter.items():
@@ -351,6 +353,11 @@ def convert_shares(task_config):
                     stderr=poly_eval_phase,
                 )
                 input_counter += player_input_count
+
+                # now look in log file for the eval point
+                if task_config.consistency_args.single_random_eval_point and eval_point is None:
+                    print("Parsing eval point")
+                    eval_point = find_eval_point(poly_eval_phase)
 
                 if task_config.sleep_time > 0:
                     print(f"Sleeping for {task_config.sleep_time} seconds to allow the process on all clients to finish.")
@@ -441,6 +448,16 @@ def copy_transaction_files(conversion_not_needed, task_config):
                 print("copying", file_name, "to", filename_suffix)
                 shutil.copyfile(os.path.join(persistence_data_path, file_name),
                                 os.path.join(persistence_data_path, filename_suffix))
+
+def find_eval_point(f):
+    eval_point_regex = r"input_consistency_player_(\d*)_eval=\((.*),(.*)\)"
+    for line in f.readlines():
+        match = re.match(eval_point_regex, line)
+        if match:
+            print("Found eval point", match.group(2))
+            return match.group(2)
+
+    raise ValueError("Could not find eval point in log file.")
 
 
 def clean_persistence_data(code_dir):
