135937fdd46dcd8195df7e293d521fa956d9c194
Hidde L <mail.hidde@gmail.com>
2024-01-18 14:06:22 +0100

    str

--- utils/python_utils/consistency_cerebro.py
+++ utils/python_utils/consistency_cerebro.py
@@ -59,7 +59,7 @@ def run_cerebro_with_args(task_config: config_def.TaskConfig, script_name: str, 
         # adjust edabits batch size
         if program_args is None:
             program_args = {}
-        program_args['b'] = 2500
+        program_args['b'] = "2500"
 
     script_runner_constr: runner_defs.ScriptBaseRunner = runner_defs.ProtocolRunners[map_protocol_to_field(task_config).name].value
     script_runner_obj = script_runner_constr(
